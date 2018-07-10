c5215b61bb01b6bc5be807aebacebdbf34bd141d
Ken <kchansf5@gmail.com>
2018-07-10 11:15:03 -0700

    Write logic to check for graph if cyclic or disconnected

--- lib/topological_sort.rb
+++ lib/topological_sort.rb
@@ -3,5 +3,32 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+    queue = Queue.new
+    checker = []
 
+    vertices.each do |v|
+        checker << v if v.in_edges != 1
+    end
+
+    return [] if checker.length > 1
+
+
+    vertices.each do |v|
+        if v.in_edges.empty?
+            queue.enq(v)
+        end
+    end
+
+    sorted = []
+    until queue.empty?
+        current = queue.pop
+        sorted << current
+
+        current.out_edges.each do |edge|
+            queue.enq(edge.to_vertex) if edge.to_vertex.in_edges.empty?
+            edge.destroy!
+        end
+        vertices.delete(current)
+    end
+    sorted
 end
